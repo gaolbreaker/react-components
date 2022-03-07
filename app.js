6137d4e0f8f84563cf9a750516b5d56a6fa167a0
Elbert Chan <elb.chan@gmail.com>
2022-03-07 14:27:24 -0800

    mini sprint done

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
+// refactor GroceryListItem to be a class component
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {hover: false};
+  }
+
+  onMouseEnter() {
+    this.setState({hover: true});
+  }
+
+  onMouseLeave() {
+    this.setState({hover: false});
+  }
+
+  render() {
+    var style = {fontWeight: this.state.hover ? 'bold' : 'normal' };
+
+    return (
+      <li style={style} onMouseEnter={this.onMouseEnter.bind(this)} onMouseLeave={this.onMouseLeave.bind(this)}>{this.props.item}</li>
+    )
+  }
+}
+
 // TODO
+
+var GroceryList = () => (
+  <ul>
+    <Eggs />
+    <Flour />
+  </ul>
+);
+
+var Eggs = (props) => (
+  <GroceryListItem item={'Eggs'}/>
+);
+
+var Flour = (props) => (
+  <GroceryListItem item={'Flour'}/>
+);
+
+// var GroceryListItem = (props) => (
+//   <li>
+//     {props.item}
+//   </li>
+// );
+
+
+ReactDOM.render(<GroceryList />, document.getElementById('app'));
+
